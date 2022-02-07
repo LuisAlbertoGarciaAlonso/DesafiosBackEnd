314bc932e7a37eced7e0cf93821bb36b52475652
Luis <luisalgar@hotmail.com>
2022-02-07 18:08:02 -0300

    desafio4

--- desafio4.js
+++ desafio4.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 
+
 class Contenedor {
   constructor(nombreArchivo) {
     this.nombreArchivo = nombreArchivo;
@@ -43,13 +44,9 @@ class Contenedor {
   }
 
   async getById(number) {
-    // Leemos el archivo
     const file = await fs.promises.readFile(this.nombreArchivo, "utf-8");
-    // Lo transformamos a un objeto de javascript(array)
     const parsedFile = JSON.parse(file);
-    // Con el metodo filter, creamos un nuevo array con el objeto que tenga el mismo id que le pasamos
     const result = parsedFile.filter((obj) => obj.id == number);
-    // Devolvemos el primer indice del array, que seria el objeto que encontramos
     if (result) {
       return result[0];
     } else {
@@ -58,21 +55,15 @@ class Contenedor {
   }
 
   async getAll() {
-    // Guardo el valor del metodo auxiliar
     await contenedor1.createIfNotExists();
     const txt = await fs.promises.readFile(this.nombreArchivo, "utf-8");
-    // Los transformo a un objeto de js para mostrarlos mejor
     return JSON.parse(txt);
   }
 
   async deleteById(id) {
-    // Guardo el valor del metodo auxiliar
     let file = await contenedor1.createIfNotExists();
-    // Lo transformo a un objeto de javascript (actualmente es un archivo de texto plano, o sea, un string)
     let parsedFile = JSON.parse(file);
-    // Devuelvo un nuevo array, esta vez con todos los objetos, MENOS el que tenga un id igual al que le paso por parametro
     const result = parsedFile.filter((obj) => obj.id != id);
-    // Reescribo el archivo con ese nuevo array transformado devuelta a string
     await fs.promises.writeFile(this.nombreArchivo, JSON.stringify(result));
     return result;
   }
@@ -105,7 +96,7 @@ const producto2 = {
 
 
 const contenedor1 = new Contenedor("./producto.txt");
-contenedor1.save(producto);
+//contenedor1.save(producto);
 const contenedor2 = new Contenedor("./producto.txt");
 //contenedor2.save(producto1);
 const contenedor3 = new Contenedor("./producto.txt");
